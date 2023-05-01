da067fe39b6b81c0b770feece454fcfbd7c12836
Aziza Baccouri <aizabaccouri@gmail.com>
2023-05-01 17:26:50 +0200

    version with range of price

--- pieces.js
+++ pieces.js
@@ -1,41 +1,42 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
-const pieces = await reponse.json();
+const pieces = await fetch("pieces-autos.json").then(pieces => pieces.json());
 
 //création des balises
+function generatePieces(pieces){
+    for (let i = 0; i < pieces.length; i++) {
 
-for (let i = 0; i < pieces.length; i++) {
-
-    const article = pieces[i];
-    // Récupération de l'élément du DOM qui accueillera les sheets
-    const sectionSheets = document.querySelector(".sheets");
-    // Création d’une balise dédiée à une pièce automobile
-    const pieceElement = document.createElement("article");
-    // Création des balises 
-    const imageElement = document.createElement("img");
-    imageElement.src = article.image;
-    const nameElement = document.createElement("h2");
-    nameElement.innerText = article.name;
-    const priceElement = document.createElement("p");
-    priceElement.innerText = `price: ${article.price} € (${article.price < 35 ? "€" : "€€€"})`;
-    const categoryElement = document.createElement("p");
-    categoryElement.innerText = article.category ?? "(aucune catégorie)";
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-    const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibility ? "En stock" : "Rupture de stock";
-    
-    // On rattache la balise article a la section Fiches
-    sectionSheets.appendChild(pieceElement);
-    // On rattache l’image à pieceElement (la balise article)
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nameElement);
-    pieceElement.appendChild(priceElement);
-    pieceElement.appendChild(categoryElement);
-    //Ajout des éléments au DOM pour l'exercice
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
- }
+        const article = pieces[i];
+        // Récupération de l'élément du DOM qui accueillera les sheets
+        const sectionSheets = document.querySelector(".sheets");
+        // Création d’une balise dédiée à une pièce automobile
+        const pieceElement = document.createElement("article");
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = article.image;
+        const nameElement = document.createElement("h2");
+        nameElement.innerText = article.name;
+        const priceElement = document.createElement("p");
+        priceElement.innerText = `price: ${article.price} € (${article.price < 35 ? "€" : "€€€"})`;
+        const categoryElement = document.createElement("p");
+        categoryElement.innerText = article.category ?? "(aucune catégorie)";
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+        const stockElement = document.createElement("p");
+        stockElement.innerText = article.disponibility ? "En stock" : "Rupture de stock";
+        
+        // On rattache la balise article a la section Fiches
+        sectionSheets.appendChild(pieceElement);
+        // On rattache l’image à pieceElement (la balise article)
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nameElement);
+        pieceElement.appendChild(priceElement);
+        pieceElement.appendChild(categoryElement);
+        //Ajout des éléments au DOM pour l'exercice
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(stockElement);
+     }
+}
+generatePieces(pieces);
 
 
 //get a reference to the button sort ascending
@@ -46,7 +47,8 @@ buttonSort.addEventListener("click", function() {
     orderedPieces.sort(function(a,b){
         return a.price - b.price;
     });
-    console.log(orderedPieces);
+    document.querySelector(".sheets").innerHTML="";
+    generatePieces(orderedPieces);
 });
 //get a reference to the button sort descending
 const buttonSortD = document.querySelector(".btn-sort-d");
@@ -56,7 +58,8 @@ buttonSortD.addEventListener("click", function() {
     orderedPieces.sort(function(a,b){
         return b.price-a.price ;
     });
-    console.log(orderedPieces);
+    document.querySelector(".sheets").innerHTML="";
+    generatePieces(orderedPieces);
 });
 
 //get reference to the button filter
@@ -66,7 +69,8 @@ buttonFilter.addEventListener("click", function(){
     const filteredPieces =pieces.filter(function(piece){
         return piece.price <= 35;
     });
-    console.log(filteredPieces);
+    document.querySelector(".sheets").innerHTML="";
+    generatePieces(filteredPieces);
 });
 
 //get reference to the button filter description
@@ -76,7 +80,8 @@ buttonFilterDescrip.addEventListener("click", function(){
     const filteredPieces =pieces.filter(function(piece){
         return piece.description ;
     });
-    console.log(filteredPieces);
+    document.querySelector(".sheets").innerHTML="";
+    generatePieces(filteredPieces);
 });
 
 // affordable pieces list
@@ -86,6 +91,9 @@ for(let i=pieces.lenght -1 ; i>=0 ; i--){
         names.splice(i,1);
     }
 }
+// header creation
+const pElement = document.createElement('p')
+pElement.innerText = "Affordable pieces";
 // list creation
 const affordablePieces = document.createElement('ul');
 // add list items
@@ -95,5 +103,15 @@ for(let i=0 ; i<names.length ; i++){
     console.log(names[i]);
     affordablePieces.appendChild(nameElement);
 }
-document.querySelector('.affordable')
+document.querySelector('.affordable').appendChild(pElement)
 .appendChild(affordablePieces);
+
+const inputMaxPrice = document.querySelector('#maxPrice');
+inputMaxPrice.addEventListener('input', function(){
+    const filteredPieces = pieces.filter(function(piece){
+        return piece.price <= inputMaxPrice.value;
+    });
+    document.querySelector(".sheets").innerHTML="";
+    generatePieces(filteredPieces);
+    });
+   
